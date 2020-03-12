0811d21e792fc8f26fa1f42275aad9dfa937111b
maeloliveira <61716327+maeloliveira@users.noreply.github.com>
2020-03-11 21:45:23 -0300

    Operações
    Criando operações aritméticas

--- calculadora.java
+++ calculadora.java
@@ -24,3 +24,23 @@ public class Calc {
 		
 	}
 	
+	public static void main (String args[]){
+		
+		//criando um objeto c a apartir do metodo calc
+		Calc c = new Calc();
+		
+		//declarando as varíaveis
+		int opcao = 5;
+		int num1;
+		int num2;
+		
+	      Scanner input = new Scanner(System.in);  
+	      
+	      
+		System.out.println("-Escolha uma opção-");
+	    System.out.println("1. Soma");  
+	    System.out.println("2. Subtracao");  
+	    System.out.println("3. Multiplicacao");  
+	    System.out.println("4. Divisao");  
+	    System.out.println("0. Sair");  
+	    System.out.println("Operação: "); 
